b8c65c746ed6e4c44861c25983a8068332b757b5
ota200 <aniscratch@gmail.com>
2022-11-23 22:29:53 -0500

    Home_Page beta v1

---
+++ src/Home.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div>
+
+      <div>
+        <h2>O.T.A</h2>
+        
+        <div>
+            <button>Home</button>
+            <button>about</button>
+            <button>projects</button>
+            <button>dark mode</button>
+        </div>
+
+      </div>
+
+      <h1> A Developer of things </h1>
+
+      <header>
+        <b><i>Same kid, who thinks becomeing a software enginer is a good life carrer.</i></b>
+      </header>
+
+      <div>
+        <button>Github</button>
+        <button>Hashnode</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Home from './Home';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
