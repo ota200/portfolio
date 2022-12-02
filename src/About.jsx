57066d2efd7bfa2676d013c1a821cda2ddd899ce
O.T.A <79542378+ota200@users.noreply.github.com>
2022-12-02 15:27:39 +0000

    Nav_bar componet

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function About() {
+  return (
+    <div className="pad">
+
+
+      <div className="bottom">
+        <h1> About </h1>
+
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Home from './Home';
+import About from './About';
+import Nav_bar from './Nav_bar';
+
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <>
+  <Nav_bar />
+  <Home />
+
+
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
